72d92352c86fbcae3e7c248b3d6ef17345829aee
Arslan Dev <arslan.dev@gmail.com>
2023-11-30 22:03:42 +0400

    Up 20 seconds

--- routes/countdown.tsx
+++ routes/countdown.tsx
@@ -2,6 +2,6 @@ import Countdown from "../islands/Countdown.tsx";
 
 export default function Page() {
   const date = new Date()
-  date.setSeconds(date.getUTCSeconds() + 10)
+  date.setSeconds(date.getUTCSeconds() + 20)
   return <p>The big event is happening <Countdown target={date.toISOString()} /></p>
 }
